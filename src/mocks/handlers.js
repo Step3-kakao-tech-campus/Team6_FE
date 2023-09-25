1c6d663c5207deafd079429985e0642ea7875343
udadai <yujeong470@gmail.com>
2023-09-26 08:38:31 +0900

    feat: add mocking

---
+++ src/mocks/handlers.js
@@ -0,0 +1,51 @@
+// mocks/handlers.js
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("/api/search", (req, res, ctx) => {
+    const query = req.url.searchParams.get("query");
+
+    if (query) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          results: {
+            restaurants: [
+              {
+                id: 1,
+                name: "정문토스트",
+                summary: "정문토스트는..",
+                image: "/image/restaurant/1",
+                address: "",
+                averageScore: 4.4,
+                liked: true,
+              },
+            ],
+            festivals: [
+              {
+                id: 1,
+                name: "2023 부산 국제 락 페스티벌",
+                summary: "부산 국제 락 페스티벌은...",
+                image: "/image/festival/1",
+                address: "",
+                averageScore: 4.4,
+                liked: true,
+              },
+            ],
+            touristSpots: [
+              {
+                id: 1,
+                name: "Signiel Busan",
+                summary: "...",
+                address: "부산 해운대구",
+                liked: true,
+              },
+            ],
+          },
+        })
+      );
+    }
+
+    return res(ctx.status(200), ctx.json({ result: {} }));
+  }),
+];
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+// pages/SearchPage.js
+import React, { useState } from "react";
 import SearchBar from "../components/molecules/SearchBar";
 
 const SearchPage = () => {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
-  const handleSearch = () => {
-    console.log(`Searching for: ${query}`);
+  const handleSearch = async () => {
+    const response = await fetch(`/api/search?query=${query}`);
+    const data = await response.json();
+    setResults(data.results);
   };
 
   return (
@@ -16,6 +20,29 @@ const SearchPage = () => {
         onChange={(e) => setQuery(e.target.value)}
         onSearch={handleSearch}
       />
+      <div>
+        <h2>Restaurants:</h2>
+        <ul>
+          {results.restaurants &&
+            results.restaurants.map((restaurant, index) => (
+              <li key={index}>{restaurant.name}</li>
+            ))}
+        </ul>
+        <h2>Festivals:</h2>
+        <ul>
+          {results.festivals &&
+            results.festivals.map((festival, index) => (
+              <li key={index}>{festival.name}</li>
+            ))}
+        </ul>
+        <h2>Tourist Spots:</h2>
+        <ul>
+          {results.touristSpots &&
+            results.touristSpots.map((spot, index) => (
+              <li key={index}>{spot.name}</li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
