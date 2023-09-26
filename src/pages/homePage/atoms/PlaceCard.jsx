2fc498a801f6740e9ffaba83ddea0a5bfb4f47d7
Dongjin <ehdwls1638@gmail.com>
2023-09-26 23:33:18 +0900

    style : 클래스네임 수정

--- src/pages/homePage/atoms/PlaceCard.jsx
+++ src/pages/homePage/atoms/PlaceCard.jsx
@@ -4,7 +4,7 @@ const PlaceCard = ({ image, name, address, to, alt }) => {
   return (
     <Link
       to={to}
-      className="spot-card w-[180px] flex-shrink-0 rounded-lg border border-gray-500 p-2"
+      className="spot-card w-[180px] flex-shrink-0 rounded-lg border border-gray-500 p-2 flex flex-col gap-1"
     >
       <div className={"image-wrapper h-[180px] w-full rounded-lg bg-gray-300 overflow-hidden"}>
         <img
@@ -13,11 +13,11 @@ const PlaceCard = ({ image, name, address, to, alt }) => {
           alt={`${alt}`}
         />
       </div>
-      <div className="spot-card__content">
-        <div className="spot-card__content__name line-clamp-2 w-full">
+      <div className={"card-info"}>
+        <div className="spot-card__name line-clamp-2 font-bold text-xl w-full">
           {name}
         </div>
-        <div className="spot-card__content__address line-clamp-2 w-full">
+        <div className="spot-card__address line-clamp-2 w-full">
           {address}
         </div>
       </div>
