f7010a8826af5b95052bba06b76d1f3566a66cbe
Dongjin <ehdwls1638@gmail.com>
2023-09-26 23:14:04 +0900

    feat : search bar 에 useInput 적용

--- src/pages/homePage/atoms/SearchBar.jsx
+++ src/pages/homePage/atoms/SearchBar.jsx
@@ -1,9 +1,13 @@
-const SearchBar = () => {
+const SearchBar = ({ value, onChange }) => {
   return (
     <input
       type="text"
       placeholder="Search"
-      className={"w-full text-lg h-[40px] rounded-3xl border-2 border-tripKoOrange py-2 px-4"}
+      className={
+        "h-[40px] w-full rounded-3xl border-2 border-tripKoOrange-300 px-4 py-2 text-lg focus:border-tripKoOrange"
+      }
+      value={value}
+      onChange={onChange}
     />
   );
 };
