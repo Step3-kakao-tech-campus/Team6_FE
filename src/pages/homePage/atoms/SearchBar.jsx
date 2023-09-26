2f62dbfd9ba4610057ea54b73b4ddae649927825
Dongjin <ehdwls1638@gmail.com>
2023-09-26 22:54:09 +0900

    feat : search bar 추가

---
+++ src/pages/homePage/atoms/SearchBar.jsx
@@ -0,0 +1,11 @@
+const SearchBar = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Search"
+      className={"w-full text-lg h-[40px] rounded-3xl border-2 border-tripKoOrange py-2 px-4"}
+    />
+  );
+};
+
+export default SearchBar;
