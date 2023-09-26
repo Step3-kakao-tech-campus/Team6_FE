cf59525a3d166f65e75967074215baa7386b1ef6
Dongjin <ehdwls1638@gmail.com>
2023-09-27 00:18:25 +0900

    feat : tailwind media query 'xs' 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xs: "550px",
+      // => @media (min-width: 550px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     colors: {
       "slate-50": "#f8fafc",
       "slate-100": "#f1f5f9",
