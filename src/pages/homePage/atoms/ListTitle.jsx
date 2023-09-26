a090bb332d10f7506b17770851b6d923f7a53a45
Dongjin <ehdwls1638@gmail.com>
2023-09-26 23:33:48 +0900

    feat : title 크기 변경

--- src/pages/homePage/atoms/ListTitle.jsx
+++ src/pages/homePage/atoms/ListTitle.jsx
@@ -1,6 +1,6 @@
 const ListTitle = ({ children }) => {
   return (
-      <h2 className={"list-title px-2 text-2xl font-bold"}>{children}</h2>
+      <h2 className={"list-title px-2 text-3xl font-extrabold"}>{children}</h2>
   );
 };
 
