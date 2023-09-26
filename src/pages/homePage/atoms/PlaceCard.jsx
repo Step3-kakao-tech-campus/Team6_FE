bce1aadfdf537da2177ce7201ffac8c8e3853522
Dongjin <ehdwls1638@gmail.com>
2023-09-27 00:21:52 +0900

    feat : PlaceCard pc화면 크기 조절

--- src/pages/homePage/atoms/PlaceCard.jsx
+++ src/pages/homePage/atoms/PlaceCard.jsx
@@ -4,11 +4,11 @@ const PlaceCard = ({ image, name, address, to, alt }) => {
   return (
     <Link
       to={to}
-      className="spot-card flex flex-shrink-0 flex-col gap-1 rounded-lg border border-gray-500 p-2 md:w-[180px]"
+      className="place-card flex flex-shrink-0 flex-col gap-1 rounded-lg border border-gray-500 p-2 md:w-[240px]"
     >
       <div
         className={
-          "image-wrapper w-full overflow-hidden rounded-lg bg-tripKoOrange-300 md:h-[180px] min-h-[180px]"
+          "image-wrapper w-full overflow-hidden rounded-lg bg-tripKoOrange-300 md:h-[240px] min-h-[180px]"
         }
       >
         <picture>
@@ -20,11 +20,11 @@ const PlaceCard = ({ image, name, address, to, alt }) => {
           />
         </picture>
       </div>
-      <div className={"card-info"}>
-        <div className="spot-card__name line-clamp-2 w-full text-xl font-bold">
+      <div className={"place-card-info"}>
+        <div className="place-card__name line-clamp-2 w-full text-xl font-bold">
           {name}
         </div>
-        <div className="spot-card__address line-clamp-2 w-full">{address}</div>
+        <div className="place-card__address line-clamp-2 w-full">{address}</div>
       </div>
     </Link>
   );
