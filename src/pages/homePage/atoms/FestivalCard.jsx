34df0a940570e68b7603ee7eaa7100e67c22b15e
Dongjin <ehdwls1638@gmail.com>
2023-09-26 23:34:37 +0900

    feat : background 생상 변경

--- src/pages/homePage/atoms/FestivalCard.jsx
+++ src/pages/homePage/atoms/FestivalCard.jsx
@@ -5,7 +5,7 @@ const FestivalCard = ({ image, to, alt }) => {
     <div className="festival-card h-[280px] w-[220px] flex-shrink-0 overflow-hidden">
       <Link to={to} className={""}>
         <img
-          className={"h-full w-full bg-yellow-500 transition-all hover:scale-105"}
+          className={"h-full w-full bg-tripKoOrange-300 transition-all hover:scale-105"}
           src={image}
           alt={alt}
         />
--- src/pages/homePage/atoms/PlaceCard.jsx
+++ src/pages/homePage/atoms/PlaceCard.jsx
@@ -6,7 +6,7 @@ const PlaceCard = ({ image, name, address, to, alt }) => {
       to={to}
       className="spot-card w-[180px] flex-shrink-0 rounded-lg border border-gray-500 p-2 flex flex-col gap-1"
     >
-      <div className={"image-wrapper h-[180px] w-full rounded-lg bg-gray-300 overflow-hidden"}>
+      <div className={"image-wrapper h-[180px] w-full rounded-lg bg-tripKoOrange-300 overflow-hidden"}>
         <img
           className={"h-full w-full hover:scale-105 transition-all"}
           src={image}
