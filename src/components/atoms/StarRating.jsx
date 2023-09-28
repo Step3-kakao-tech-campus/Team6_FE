99f7dfd1a136474bffb18d7d3eef2c61c69c46e7
udadai <yujeong470@gmail.com>
2023-09-28 23:23:15 +0900

    style: star rating 정렬

---
+++ src/components/atoms/StarRating.jsx
@@ -0,0 +1,24 @@
+import { FaStar } from "react-icons/fa";
+
+const StarRating = ({ averageScore }) => {
+  const renderRatingStars = () => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= averageScore) {
+        stars.push(
+          <FaStar key={i} color="#ffc107" style={{ display: "inline-block" }} />
+        );
+      } else {
+        stars.push(
+          <FaStar key={i} color="#e4e5e9" style={{ display: "inline-block" }} />
+        );
+      }
+    }
+
+    return stars;
+  };
+
+  return <div className="text-orange-500">{renderRatingStars()}</div>;
+};
+
+export default StarRating;
--- src/components/molecules/RestaurantCard.jsx
+++ src/components/molecules/RestaurantCard.jsx
@@ -1,24 +1,10 @@
-import { FaStar } from "react-icons/fa";
+import StarRating from "../atoms/StarRating";
 
 const RestaurantCard = ({ children, averageScore }) => {
-  const renderingRatingStars = () => {
-    const stars = [];
-    for (let i = 1; i <= 5; i++) {
-      if (i <= averageScore) {
-        stars.push(<FaStar key={i} color="#ffc107" />);
-      } else {
-        stars.push(<FaStar key={i} color="#e4e5e9" />);
-      }
-    }
-
-    return stars;
-  };
-
   return (
-    <div className="rounded-tr-lg rounded-br-lg shadow-md bg-white p-4 m-2 text-xl">
+    <div className="rounded-tr-2xl rounded-br-2xl shadow-md bg-white p-4 m-2 text-xl">
       {children}
-      <br />
-      <div className="text-orange-500">{renderingRatingStars()}</div>
+      <StarRating averageScore={averageScore} />
     </div>
   );
 };
