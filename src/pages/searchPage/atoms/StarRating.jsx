b5085bb1e131961ee7615b625e3ffa89303031f5
udadai <yujeong470@gmail.com>
2023-09-28 23:58:01 +0900

    feat: Festival card 별도로 추가

--- src/pages/searchPage/atoms/StarRating.jsx
+++ src/pages/searchPage/atoms/StarRating.jsx
@@ -6,11 +6,19 @@ const StarRating = ({ averageScore }) => {
     for (let i = 1; i <= 5; i++) {
       if (i <= averageScore) {
         stars.push(
-          <FaStar key={i} color="#ffc107" style={{ display: "inline-block" }} />
+          <FaStar
+            key={i}
+            color="#ffc107"
+            style={{ display: "inline-block", fontSize: "16px" }}
+          />
         );
       } else {
         stars.push(
-          <FaStar key={i} color="#e4e5e9" style={{ display: "inline-block" }} />
+          <FaStar
+            key={i}
+            color="#e4e5e9"
+            style={{ display: "inline-block", fontSize: "16px" }}
+          />
         );
       }
     }
