08f4aa26843491261b77f436572894eed9349889
Dongjin <ehdwls1638@gmail.com>
2023-10-02 17:22:31 +0900

    fix : 이미지 늘어남 해결

--- src/pages/homePage/atoms/FestivalCard.jsx
+++ src/pages/homePage/atoms/FestivalCard.jsx
@@ -5,7 +5,7 @@ const FestivalCard = ({ image, to, alt }) => {
     <div className="festival-card h-[420px] w-[300px] flex-shrink-0 overflow-hidden">
       <Link to={to} className={""}>
         <img
-          className={"h-full w-full bg-tripKoOrange-300 transition-all hover:scale-105"}
+          className={"h-full w-full bg-tripKoOrange-300 transition-all hover:scale-105 object-cover"}
           src={image}
           alt={alt}
         />
