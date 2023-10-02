0145a584863d1032721fc4da5d2f68117895bb30
Dongjin <ehdwls1638@gmail.com>
2023-10-02 14:08:00 +0900

    feat : mocking data 추가

---
+++ src/mocks/datas/restaurants.js
@@ -0,0 +1,43 @@
+const RESTAURANTS = [
+  {
+    id: 1,
+    name: "Pusan National University Main Gate Toast",
+    summary:
+      "Jeongmun Toast is an old toast house famous among Pusan National University students. this store was dis...",
+    description:
+      "Jeongmun Toast is an old toast house famous among Pusan National University students. this store was discovered by a student who was looking for a place to eat after a night of drinking. The store is located near the main gate of Pusan National University, and it is a popular place for students to eat after a night of drinking. The store is open 24 hours a day, and it is a popular place for students to eat after a night of drinking. The store is open 24 hours a day, and it is a popular place for students to eat after a night of drinking. The store is open 24 hours a day, and it is a popular place for students to eat after a night of drinking. The store is open 24 hours a day, and it is a popular place for students to eat after a night of drinking. The store is open 24 hours a day, and it is a popular place for students to eat after a night of drinking.",
+    mainImage: "https://picsum.photos/205",
+    images: [
+      "https://picsum.photos/200",
+      "https://picsum.photos/202",
+      "https://picsum.photos/203",
+      "https://picsum.photos/204",
+    ],
+    address: "Pusan National University, Pusan",
+    averageRating: 4.4,
+    holidayDate: "2021-10-10",
+    open: "11:00~22:00",
+    breakTime: "15:00~17:00",
+    contactInfo: "010-1234-5678",
+    reservable: true,
+    liked: true,
+    manus: [
+      {
+        id: 1,
+        name: "toast",
+        image: "https://picsum.photos/300",
+        price: 3000,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in tincidunt metus. Proin dictum nunc nisi, non consectetur dui posuere sed. Pellentesque eget lobortis metus. Sed vitae justo id lorem blandit dictum at congue sapien. Fusce erat orci, fermentum ut congue quis, pharetra ut mauris. Nam ornare ex id ante maximus maximus. Morbi sodales tortor non turpis iaculis, ut condimentum nunc eleifend. Nullam pretium quam vitae lorem cursus laoreet tincidunt nec mauris.",
+      },
+      {
+        id: 2,
+        name: "toast",
+        image: "https://picsum.photos/300",
+        price: 3000,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in tincidunt metus. Proin dictum nunc nisi, non consectetur dui posuere sed. Pellentesque eget lobortis metus. Sed vitae justo id lorem blandit dictum at congue sapien. Fusce erat orci, fermentum ut congue quis, pharetra ut mauris. Nam ornare ex id ante maximus maximus. Morbi sodales tortor non turpis iaculis, ut condimentum nunc eleifend. Nullam pretium quam vitae lorem cursus laoreet tincidunt nec mauris.",
+      },
+    ],
+  },
+];
+
+export default RESTAURANTS;
