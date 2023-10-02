6b419492b61b80a5d1dbee099ab421360fb5e654
Dongjin <ehdwls1638@gmail.com>
2023-10-02 14:09:27 +0900

    refactor : in component mock data 삭제

--- src/pages/homePage/molecules/FestivalCardList.jsx
+++ src/pages/homePage/molecules/FestivalCardList.jsx
@@ -2,35 +2,6 @@ import FestivalCard from "../atoms/FestivalCard";
 import HorizontalListWrapper from "../atoms/HorizontalListWrapper";
 
 const FestivalCardList = ({ festivals }) => {
-  festivals = [
-    {
-      id: 1,
-      name: "2023 부산 국제 락 페스티벌",
-      summary: "부산 국제 락 페스티벌은...",
-      image: "/image/festival/1",
-      address: "",
-      averageScore: 4.4,
-      liked: true,
-    },
-    {
-      id: 2,
-      name: "2023 부산 국제 락 페스티벌",
-      summary: "부산 국제 락 페스티벌은...",
-      image: "/image/festival/1",
-      address: "",
-      averageScore: 4.4,
-      liked: true,
-    },
-    {
-      id: 3,
-      name: "2023 부산 국제 락 페스티벌",
-      summary: "부산 국제 락 페스티벌은...",
-      image: "/image/festival/1",
-      address: "",
-      averageScore: 4.4,
-      liked: true,
-    },
-  ];
   return (
     <HorizontalListWrapper>
       {festivals.map((festival) => {
