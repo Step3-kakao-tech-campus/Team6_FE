0236f834b115aed90db4717640dcaad6ece1b01c
Dongjin <ehdwls1638@gmail.com>
2023-10-02 14:09:27 +0900

    refactor : in component mock data 삭제

--- src/pages/homePage/molecules/RestaurantCardList.jsx
+++ src/pages/homePage/molecules/RestaurantCardList.jsx
@@ -2,35 +2,6 @@ import HorizontalListWrapperMediaQuery from "../atoms/HorizontalListWrapperMedia
 import PlaceCard from "../atoms/PlaceCard";
 
 const RestaurantCardList = ({ restaurants }) => {
-  restaurants = [
-    {
-      id: 1,
-      name: "정문토스트",
-      summary: "정문토스트는..",
-      image: "/image/restaurant/1", // 필요없을 수 있다. id로 대체되기 때문에
-      address: "장소",
-      averageScore: 4.4,
-      liked: true,
-    },
-    {
-      id: 2,
-      name: "정문토스트",
-      summary: "정문토스트는..",
-      image: "/image/restaurant/1", // 필요없을 수 있다. id로 대체되기 때문에
-      address: "장소 좀 많이 긴 장소",
-      averageScore: 4.4,
-      liked: true,
-    },
-    {
-      id: 3,
-      name: "정문토스트 정문 토스트 정문 토스트 정문 토스트 정문 토스트",
-      summary: "정문토스트는..",
-      image: "/image/restaurant/1", // 필요없을 수 있다. id로 대체되기 때문에
-      address: "장소 아주 많이 긴 장소 아주 많이 긴 장소",
-      averageScore: 4.4,
-      liked: true,
-    },
-  ];
   return (
     <HorizontalListWrapperMediaQuery>
       {restaurants.map((restaurant) => {
--- src/pages/homePage/molecules/SpotCardList.jsx
+++ src/pages/homePage/molecules/SpotCardList.jsx
@@ -2,33 +2,6 @@ import PlaceCard from "../atoms/PlaceCard";
 import HorizontalListWrapperMediaQuery from "../atoms/HorizontalListWrapperMediaQuery";
 
 const SpotCardList = ({ spots }) => {
-  spots = [
-    {
-      id: 1,
-      name: "Signiel Busan",
-      summary: "...",
-      image: "image/touringSpot/1",
-      address: "부산 해운대구",
-      liked: true,
-    },
-    {
-      id: 2,
-      name: "Signiel Busan",
-      summary: "...",
-      image: "image/touringSpot/1",
-      address: "부산 해운대구",
-      liked: true,
-    },
-    {
-      id: 3,
-      name: "Signiel Busan",
-      summary: "...",
-      image: "image/touringSpot/1",
-      address: "부산 해운대구",
-      liked: true,
-    },
-  ];
-
   return (
     <HorizontalListWrapperMediaQuery>
       {spots.map((spot) => (
