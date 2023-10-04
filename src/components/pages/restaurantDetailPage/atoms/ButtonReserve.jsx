0c8ffbc0881f3063b79723f50cee0e703d0e8bfe
Dongjin <ehdwls1638@gmail.com>
2023-10-04 18:14:12 +0900

    feat : 예약 버튼 스타일 수정

--- src/components/pages/restaurantDetailPage/atoms/ButtonReserve.jsx
+++ src/components/pages/restaurantDetailPage/atoms/ButtonReserve.jsx
@@ -1,7 +1,7 @@
 const ButtonReserve = ({ onClick, enable }) => {
   return (
     <button
-      className={`width-flex-layout fixed bottom-[74px] text-2xl h-12 rounded-lg font-bold text-white ${
+      className={`width-flex-layout fixed bottom-[74px] text-2xl h-12 rounded-3xl font-bold text-white ${
         enable ? "bg-tripKoOrange" : "bg-gray-400"
       }`}
       onClick={onClick}
