9b6c36b4085eaf215478de0bd0bd0eb92cfe5fb1
Dongjin <ehdwls1638@gmail.com>
2023-10-05 11:19:21 +0900

    feat : review section 작성

---
+++ src/components/pages/restaurantDetailPage/atoms/ButtonAllReviews.jsx
@@ -0,0 +1,10 @@
+const ButtonAllReviews = () => {
+  return (
+    <button
+      className={"text-lg font-bold text-tripKoOrange hover:text-tripKoOrange-500"}>
+      See all reviews
+    </button>
+  );
+};
+
+export default ButtonAllReviews;
