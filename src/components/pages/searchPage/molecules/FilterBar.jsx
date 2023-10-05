7ccc774ff8b5be6379c5653a12f27cfd1a3f6ab0
Dongjin <ehdwls1638@gmail.com>
2023-10-05 11:43:40 +0900

    refactor : 불필요한 import 삭제

--- src/components/pages/searchPage/molecules/FilterBar.jsx
+++ src/components/pages/searchPage/molecules/FilterBar.jsx
@@ -1,5 +1,5 @@
 import FilterButton from "../../../common/atoms/FilterButton";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const FilterBar = ({ filter, setFilter }) => {
   const navigate = useNavigate();
