0242e30aff61effe734e9bb333c0ff45f50a0223
Dongjin <ehdwls1638@gmail.com>
2023-10-07 17:52:25 +0900

    feat : ReviewCards에 see all review 통합

--- src/components/features/festivalDetailPage/FestivalDetailTemplate.jsx
+++ src/components/features/festivalDetailPage/FestivalDetailTemplate.jsx
@@ -1,13 +1,15 @@
 import PageTitleBar from "../../molecules/PageTitleBar";
 import SectionTitle from "../../atoms/SectionTitle";
 import InfoElement from "../restaurantDetailPage/atoms/InfoElement";
-import {comma} from "../../../utils/convert";
+import { comma } from "../../../utils/convert";
 import ReviewCards from "../../organisms/ReviewCards";
 import Carousel from "../carousel/Carousel";
-import {imagesToSlides} from "../carousel/utils";
+import { imagesToSlides } from "../carousel/utils";
 import ButtonAllReviews from "../restaurantDetailPage/atoms/ButtonAllReviews";
+import {useState} from "react";
 
 const FestivalDetailTemplate = ({ festival }) => {
+    const [isActiveReview, setIsActiveReview] = useState(false);
   return (
     <>
       <PageTitleBar name={festival.name} />
@@ -22,27 +24,23 @@ const FestivalDetailTemplate = ({ festival }) => {
           className={"w-full"}
         />
       </div>
-      <div className={"festival-detail-content mt-[50rem] pb-[8rem] bg-white"}>
+      <div className={"festival-detail-content mt-[50rem] bg-white pb-[8rem]"}>
         <SectionTitle title={"Information"} />
         <div className={"detail-content-container px-2"}>
           {festival.description}
         </div>
-        <div
-          className={
-            "infomation-card grid gap-2 px-4 py-2 md:grid-cols-2"
-          }
-        >
-            <InfoElement title={"Address"} value={festival.address} />
-            <InfoElement title={"Period"} value={festival.period} />
+        <div className={"information-card grid gap-2 px-4 py-2 md:grid-cols-2"}>
+          <InfoElement title={"Address"} value={festival.address} />
+          <InfoElement title={"Period"} value={festival.period} />
           <InfoElement title={"Price"} value={`₩${comma(festival.price)}`} />
         </div>
-          <SectionTitle title={"Photo"} />
-          <div className={"carousel-wrapper height-flex-layout-medium"}>
-              <Carousel slides={imagesToSlides(festival.images)} />
-          </div>
-          <SectionTitle title={"Reviews"} />
-          <ReviewCards placeId={festival.id} count={4} />
-          <ButtonAllReviews placeId={festival.id} placeType={"festival"}/>
+        <SectionTitle title={"Photo"} />
+        <div className={"carousel-wrapper height-flex-layout-medium"}>
+          <Carousel slides={imagesToSlides(festival.images)} />
+        </div>
+        <SectionTitle title={"Reviews"} />
+        <ReviewCards placeId={festival.id} placeType={"festival"} count={isActiveReview?0:4} />
+          {!isActiveReview && <ButtonAllReviews onClick={() => setIsActiveReview(true)} />}
       </div>
     </>
   );
--- src/components/features/restaurantDetailPage/atoms/ButtonAllReviews.jsx
+++ src/components/features/restaurantDetailPage/atoms/ButtonAllReviews.jsx
@@ -1,11 +1,8 @@
-import {useNavigate} from "react-router-dom";
-
-const ButtonAllReviews = ({placeType, placeId}) => {
-    const navigate = useNavigate();
+const ButtonAllReviews = ({onClick}) => {
   return (
     <button
       className={"text-lg font-bold text-tripKoOrange hover:text-tripKoOrange-500"}
-      onClick={() => navigate(`/${placeType}/reviews/${placeId}`)}
+      onClick={onClick}
     >
       See all reviews
     </button>
