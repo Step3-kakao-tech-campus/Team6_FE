377b0c0a1b39ff4cc0233a1d65326adeb07a9787
Dongjin <ehdwls1638@gmail.com>
2023-10-07 15:13:11 +0900

    fix : resolve infinity loop rendering

--- src/components/features/festivalDetailPage/FestivalDetailPage.jsx
+++ src/components/features/festivalDetailPage/FestivalDetailPage.jsx
@@ -1,13 +1,14 @@
 import {useParams} from "react-router-dom";
 import {useQuery} from "react-query";
 import {getFestivalById} from "../../../apis/detail";
+import FestivalDetailTemplate from "./FestivalDetailTemplate";
 
 const FestivalDetailPage = () => {
   const params = useParams().id;
   const {data} = useQuery(`festival${params}`, () => getFestivalById(params));
     return (
         <div className={"festival-detail-page w-full"}>
-            {data && <FestivalDetailPage festival={data.result} />}
+            {data && <FestivalDetailTemplate festival={data.result} />}
         </div>
   );
 };
