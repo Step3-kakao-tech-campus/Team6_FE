56336e236191f96ac897c56655d45580fe48e1b1
udadai <yujeong470@gmail.com>
2023-10-07 14:34:11 +0900

    style: filter 제거

--- src/apis/search.js
+++
@@ -1,9 +0,0 @@
-export const search = async (query, setResults) => {
-  try {
-    const response = await fetch(`/api/search?query=${query}`);
-    const data = await response.json();
-    setResults(data.results);
-  } catch (error) {
-    console.error("Error performing search:", error);
-  }
-};
--- src/components/pages/searchPage/organisms/FilterResults.jsx
+++ src/components/pages/searchPage/organisms/FilterResults.jsx
@@ -2,7 +2,7 @@ import RestaurantCard from "../../../common/molecules/RestaurantCard";
 import FestivalCard from "../../../common/molecules/FestivalCard";
 import TouristSpotCard from "../../../common/molecules/TouristSpotCard";
 
-const FilterResults = ({ filter, results, query }) => {
+const FilterResults = ({ results, filter, query }) => {
   return (
     <div>
       {filter === "all" || filter === "restaurants" ? (
@@ -10,11 +10,9 @@ const FilterResults = ({ filter, results, query }) => {
           <h2 className="mx-4 text-xl font-bold">Restaurants</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2">
             {results.restaurants &&
-              results.restaurants
-                .filter((restaurant) => restaurant.address.includes(query))
-                .map((restaurant, index) => (
-                  <RestaurantCard key={index} restaurant={restaurant} />
-                ))}
+              results.restaurants.map((restaurant, index) => (
+                <RestaurantCard key={index} restaurant={restaurant} />
+              ))}
           </div>
         </div>
       ) : null}
@@ -23,11 +21,9 @@ const FilterResults = ({ filter, results, query }) => {
           <h2 className="mx-4 text-xl font-bold">Festivals</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2">
             {results.festivals &&
-              results.festivals
-                .filter((festival) => festival.address.includes(query))
-                .map((festival, index) => (
-                  <FestivalCard key={index} festival={festival} />
-                ))}
+              results.festivals.map((festival, index) => (
+                <FestivalCard key={index} festival={festival} />
+              ))}
           </div>
         </div>
       ) : null}
@@ -36,11 +32,9 @@ const FilterResults = ({ filter, results, query }) => {
           <h2 className="mx-4 text-xl font-bold">Tourist Spots</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
             {results.touristSpots &&
-              results.touristSpots
-                .filter((touristSpot) => touristSpot.address.includes(query))
-                .map((touristSpot, index) => (
-                  <TouristSpotCard key={index} touristSpot={touristSpot} />
-                ))}
+              results.touristSpots.map((touristSpot, index) => (
+                <TouristSpotCard key={index} touristSpot={touristSpot} />
+              ))}
           </div>
         </div>
       ) : null}
