6dde7f212519994393dce0e552840e02b2deb701
Dongjin <ehdwls1638@gmail.com>
2023-10-07 15:13:32 +0900

    style : resize nav bar indicator

--- src/components/layouts/navbar/NavBarIndicator/NavIndicator.jsx
+++ src/components/layouts/navbar/NavBarIndicator/NavIndicator.jsx
@@ -3,7 +3,7 @@ const NavIndicator = ({ activatedTab }) => {
     <div className="navbar-indicator w-1/4 h-1 absolute top-0 flex justify-center items-center transition-all duration-200 ease-in-out"
          style={{ left: `${activatedTab * 25 - 25}%` }}>
       <div
-        className="navbar-indicator-line bg-tripKoOrange-500 w-full h-full"
+        className="navbar-indicator-line bg-tripKoOrange-500 w-4/5 h-full"
       ></div>
     </div>
   );
