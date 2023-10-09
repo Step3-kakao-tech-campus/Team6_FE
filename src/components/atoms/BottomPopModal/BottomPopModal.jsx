89c3d601da6cb5c488f3ba297c98ef8a6c740948
Dongjin <ehdwls1638@gmail.com>
2023-10-09 14:27:23 +0900

    feat : add bottom pop modal

---
+++ src/components/atoms/BottomPopModal/BottomPopModal.jsx
@@ -0,0 +1,44 @@
+import { AiOutlineClose } from "react-icons/ai";
+import { useCallback, useEffect, useState } from "react";
+import { lockBodyScroll } from "./utils";
+
+const BottomPopModal = ({ isActive, onClose, children }) => {
+
+  const [opened, setOpened] = useState(false);
+
+  useEffect(() => lockBodyScroll(), []);
+  useEffect(() => {
+    setOpened(true);
+  }, []);
+
+  const onCloseModal = useCallback(() => {
+    setOpened(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [onClose]);
+
+  return (
+    <>
+      {isActive && (
+        <>
+          <div className={opened ? "modal-background" : "modal-background__hidden"}></div>
+          <div className={opened ? "bottom-modal-board__opened" : "bottom-modal-board__closed"}>
+            <div className={"modal-board-header width-flex-layout fixed z-[2] w-full rounded-t-3xl bg-white p-4"}>
+              <button
+                className={"modal-board-close-button flex items-center justify-center"}
+                onClick={onCloseModal}
+              >
+                <AiOutlineClose size={30} />
+              </button>
+            </div>
+            <div className={"modal-board-body pt-16"}></div>
+            {children}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default BottomPopModal;
