2071e8fd7501ddbdd4c624facbce386d42050e9e
Dongjin <ehdwls1638@gmail.com>
2023-10-12 18:59:33 +0900

    refactor : delete useless useEffect()

--- src/components/features/calendar/Calendar.jsx
+++ src/components/features/calendar/Calendar.jsx
@@ -3,10 +3,7 @@ import SectionTitle from "../../atoms/SectionTitle";
 import CalendarSlide from "./CalendarSlide";
 
 const Calendar = ({ selectedDate, setSelectedDate, unavailableDays }) => {
-  const [monthState, setMonthState] = useState(new Date());
-  useEffect(() => { // TODO : 이 부분이 없을 경우, 처음에는 달력이 제대로 렌더링되지 않는다.
-    setMonthState(new Date(monthState.getFullYear(), monthState.getMonth()));
-  }, []);
+  const [monthState, setMonthState] = useState(new Date(new Date().getFullYear(), new Date().getMonth()));
   return (
     <div className={"calender flex flex-col"}>
       <SectionTitle title={"Select date to visit"} />
