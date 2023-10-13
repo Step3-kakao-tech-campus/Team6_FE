2c5f67a3ccacd88e21704cc773dc8a7bdfe05eb7
udadai <yujeong470@gmail.com>
2023-10-13 15:45:45 +0900

    style: foodSearchPage background 스타일 수정

--- src/components/features/foodSearchPage/FoodSearchPage.jsx
+++ src/components/features/foodSearchPage/FoodSearchPage.jsx
@@ -26,22 +26,26 @@ const FoodSearchPage = () => {
   };
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center overflow-y-auto">
-      <div className="bg-food-search h-full w-full flex-col items-center justify-center overflow-y-auto">
-        <div className="w-full bg-cover bg-center backdrop-blur-sm">
-          <div
-            className={`input w-full transition-all ${
-              results.length > 0 ? "mt-6 flex" : "h-screen"
-            }`}
-          >
-            {results?.length === 0 && (
-              <h1 className="mb-4 text-center text-6xl font-extrabold">
-                Find Korea only for you
-              </h1>
-            )}
-            <FiSearch className="absolute left-8 top-3 text-gray-500" />
+    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-y-auto">
+      <div className="bg-food-search absolute inset-0 top-0 z-0 h-full bg-cover bg-center bg-no-repeat brightness-50"></div>
+
+      <div className="absolute top-0 flex w-full flex-col items-center justify-center ">
+        <div
+          className={`input flex w-full transition-all ${
+            results.length > 0
+              ? "mt-6"
+              : "h-[calc(100vh-66px)] flex-col items-center justify-center"
+          }`}
+        >
+          {results?.length === 0 && (
+            <h1 className="mb-4 text-center text-6xl font-extrabold text-white">
+              Find Korea only for you
+            </h1>
+          )}
+          <div className="relative mx-4 flex w-full flex-col">
+            <FiSearch className="absolute left-6 top-3 text-gray-500" />
             <Input
-              className="text-md mx-4 w-full rounded-lg p-2 pl-10 outline-none"
+              className="text-md w-full rounded-lg bg-gray-100 p-2 pl-16 outline-none"
               placeholder="Search for food"
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
@@ -52,30 +56,28 @@ const FoodSearchPage = () => {
               }}
             />
           </div>
-          {results?.length > 0 && currentQuery && (
-            <h3 className="mx-4 mt-2 text-xl">
-              Searching for "{currentQuery}"
-            </h3>
-          )}
-          <div className="food-list grid w-full grid-cols-1 gap-0 md:grid-cols-2">
-            {results?.map((food) => (
-              <Link key={food.id} to={`/foods/${food.id}`}>
-                <div
-                  key={food.id}
-                  className="food-card shadow-rounded-card mx-4 my-2 items-center bg-white p-4"
-                >
-                  <img
-                    src={food.image}
-                    alt={food.name}
-                    className="mb-2 h-36 w-full rounded-md object-cover"
-                  />
-                  <span className="text-sm text-gray-500">{food.category}</span>
-                  <h2 className="my-1 text-2xl font-bold">{food.name}</h2>
-                  <p className="mb-4 text-gray-500 clamp-3">{food.summary}</p>
-                </div>
-              </Link>
-            ))}
-          </div>
+        </div>
+        {results?.length > 0 && currentQuery && (
+          <h3 className="mx-4 mt-2 text-xl">Searching for "{currentQuery}"</h3>
+        )}
+        <div className="food-list grid w-full grid-cols-1 gap-0 md:grid-cols-2">
+          {results?.map((food) => (
+            <Link key={food.id} to={`/foods/${food.id}`}>
+              <div
+                key={food.id}
+                className="food-card shadow-rounded-card mx-4 my-2 items-center bg-white p-4"
+              >
+                <img
+                  src={food.image}
+                  alt={food.name}
+                  className="mb-2 h-36 w-full rounded-md object-cover"
+                />
+                <span className="text-sm text-gray-500">{food.category}</span>
+                <h2 className="my-1 text-2xl font-bold">{food.name}</h2>
+                <p className="mb-4 text-gray-500 clamp-3">{food.summary}</p>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
