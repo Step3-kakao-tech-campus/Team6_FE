c8bb3a567320a0dd3dc500933cfd3cdf2fd239d7
udadai <yujeong470@gmail.com>
2023-10-20 16:07:21 +0900

    style: PageTitle align center 적용

--- src/components/features/wishlistPage/WishlistPage.jsx
+++ src/components/features/wishlistPage/WishlistPage.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Button from "../../atoms/Button";
+import PageTitleBar from "../../molecules/PageTitleBar";
+import FilterBar from "../../molecules/FilterBar";
 
 const WishlistPage = () => {
+  const [filter, setFilter] = useState("all");
+
   return (
-    <div>
-      <h1>WishlistPage</h1>
+    <div className="wishlist-page w-full">
+      <PageTitleBar name={"Wishlist"} />
+      <div className="filter-bar mt-20">
+        <FilterBar filter={filter} setFilter={setFilter} />
+      </div>
       <Button as="a" href="/" variant="secondary">
         Go to Home
       </Button>
