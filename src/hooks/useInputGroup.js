fddc9cbe753fe35f6fd8d371f58aa8180c01430b
Dongjin <ehdwls1638@gmail.com>
2023-10-31 01:09:13 +0900

    feat : add input hook

--- src/hooks/useInputGroup.js
+++ src/hooks/useInputGroup.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+/**
+ * InputRroup을 사용하기 위한 커스텀 훅
+ * @param initialValue {string} 초기값
+ * @param validateFunction {function} 유효성 검사 함수 [ (value) => { return value.length > 0 ? "" : "required." } ]
+ * @returns {[string,onChange,string,(function(): boolean),((value: (((prevState: string) => string) | string)) => void)]}
+ */
 const useInputGroup = (initialValue, validateFunction) => {
   const [value, setValue] = useState(initialValue);
   const [errorMsg, setErrorMsg] = useState("");
@@ -12,7 +18,7 @@ const useInputGroup = (initialValue, validateFunction) => {
     setValue(e.target.value);
   };
 
-  return [value, onChange, errorMsg, validate];
+  return [value, onChange, errorMsg, validate, setErrorMsg];
 };
 
 export default useInputGroup;
