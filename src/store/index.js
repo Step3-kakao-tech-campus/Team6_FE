0e387e1e05efbb4e7ea72c8657233d240e3b5e8d
Dongjin <ehdwls1638@gmail.com>
2023-10-30 21:06:38 +0900

    feat : reconstruct store, slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import navStateReducer from "./navStateSlice";
+import navStateReducer from "./slice/navStateSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
