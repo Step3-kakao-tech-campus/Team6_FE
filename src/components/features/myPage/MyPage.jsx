6cb1f9b8acc964afb273a339be0a8020b01f6314
udadai <yujeong470@gmail.com>
2023-11-02 01:00:00 +0900

    style: MyPage 간단 스타일

--- src/components/features/myPage/MyPage.jsx
+++ src/components/features/myPage/MyPage.jsx
@@ -1,8 +1,10 @@
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import Button from "../../atoms/Button";
-import UserAvatar from "../../atoms/UserAvatar";
 import { user } from "../../../apis/user";
+import { AiOutlineHome } from "react-icons/ai";
+import { IoSettingsOutline } from "react-icons/io5";
+import Photo from "../../atoms/Photo";
 
 const MyPage = () => {
   const { data, isLoading, error } = useQuery("user", user);
@@ -11,29 +13,48 @@ const MyPage = () => {
   console.log(userDetails);
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <Button as={Link} to="/" className="mb-4">
-        Home
-      </Button>
-
-      <div>
-        <UserAvatar
-          image={"https://picsum.photos/230"}
-          className="rounded-full"
-        />
-        <div className="text-lg font-bold">{userDetails?.nickName}</div>
-        <div>{userDetails?.name}</div>
-        <div>{userDetails?.email}</div>
+    <div className="userProfile-container flex h-screen flex-col items-center">
+      <div className="userProfile-navigation mt-4 flex">
+        <Button as={Link} to="/" className="mb-4">
+          <AiOutlineHome size={24} />
+        </Button>
+        <Button as={Link} to="/userinfo/settings" className="mb-4 ml-4">
+          <IoSettingsOutline size={24} />
+        </Button>
       </div>
 
-      <Button as={Link} to="/my-page/edit">
+      <div className="userProfile-infoSection flex flex-col items-center justify-center">
+        <Photo
+          src={"https://picsum.photos/230"}
+          className="userProfile-image my-2 rounded-full"
+        />
+
+        <div className="userProfile-nickName my-2 text-2xl font-bold">
+          {userDetails?.nickName}
+        </div>
+
+        <div className="userProfile-email mb-2">
+          <span className="text-gray-400">email: {userDetails?.email}</span>
+        </div>
+      </div>
+
+      <Button
+        as={Link}
+        to="/userinfo/edit"
+        className="userProfile-editButton mb-2 text-lg text-[#FF4800] underline underline-offset-4"
+      >
         Edit Profile
       </Button>
 
-      <div className="flex flex-col items-center justify-center">
+      <div className="userProfile-actions flex flex-col items-center justify-center">
         <Button as={Link} to="/userinfo/wishlist/all">
-          Wishlist
+          My Wishlist
         </Button>
+
+        <Button as={Link} to="/userinfo/reviews">
+          My Reviews
+        </Button>
+
         <Button as={Link} to="/userinfo/reservations/restaurant">
           My Reservations
         </Button>
