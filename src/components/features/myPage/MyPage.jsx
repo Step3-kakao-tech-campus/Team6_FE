e1579e3143c5b9c305ec3fe01def4f10943be71b
udadai <yujeong470@gmail.com>
2023-11-03 18:23:17 +0900

    refactory: add MyPageTemplate

--- src/components/features/myPage/MyPage.jsx
+++ src/components/features/myPage/MyPage.jsx
@@ -1,74 +1,19 @@
 import { useQuery } from "react-query";
-import { Link } from "react-router-dom";
-import Button from "../../atoms/Button";
 import { user } from "../../../apis/user";
-import { AiOutlineHome } from "react-icons/ai";
-import { IoSettingsOutline } from "react-icons/io5";
-import Photo from "../../atoms/Photo";
+import MyPageTemplate from "./MyPageTemplate";
 
 const MyPage = () => {
   const { data, isLoading, error } = useQuery("user", user);
 
   const userDetails = data?.data?.response;
-  console.log(userDetails);
+  // console.log(userDetails);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error occurred: {error.message}</div>;
 
   return (
     <div className="userProfile-container flex h-screen w-full flex-col items-center overflow-y-auto pb-20">
-      <div className="userProfile-navigation mt-4 flex">
-        <Button as={Link} to="/" className="mb-4">
-          <AiOutlineHome size={24} />
-        </Button>
-        <Button as={Link} to="/userinfo/settings" className="mb-4 ml-4">
-          <IoSettingsOutline size={24} />
-        </Button>
-      </div>
-
-      <div className="userProfile-infoSection flex flex-col items-center justify-center">
-        <Photo
-          src={"https://picsum.photos/230"}
-          className="userProfile-image my-2 h-60 w-60 rounded-full object-cover"
-        />
-
-        <div className="userProfile-nickName my-2 text-2xl font-bold">
-          {userDetails?.nickName}
-        </div>
-
-        <div className="userProfile-email mb-2">
-          <span className="text-gray-400">email: {userDetails?.email}</span>
-        </div>
-      </div>
-
-      <Button
-        as={Link}
-        to="/userinfo/edit"
-        className="userProfile-editButton mb-2 text-lg text-[#FF4800] underline underline-offset-4"
-      >
-        Edit Profile
-      </Button>
-
-      <div className="userProfile-actions mt-4 flex flex-col items-center justify-center">
-        <Button
-          as={Link}
-          to="/userinfo/wishlist/all"
-          className="mb-2 flex h-10 w-60 items-center justify-center rounded-md shadow-md"
-        >
-          My Wishlist
-        </Button>
-        <Button
-          as={Link}
-          to="/userinfo/reviews"
-          className="mb-2 flex h-10 w-60 items-center justify-center rounded-md shadow-md"
-        >
-          My Reviews
-        </Button>
-        <Button
-          as={Link}
-          to="/userinfo/reservations/restaurant"
-          className="mb-2 flex h-10 w-60 items-center justify-center rounded-md shadow-md"
-        >
-          My Reservations
-        </Button>
-      </div>
+      <MyPageTemplate userDetails={userDetails} />
     </div>
   );
 };
---
+++ src/components/features/myPage/MyPageTemplate.jsx
@@ -0,0 +1,67 @@
+import Button from "../../atoms/Button";
+import { Link } from "react-router-dom";
+import Photo from "../../atoms/Photo";
+import { AiOutlineHome } from "react-icons/ai";
+import { IoSettingsOutline } from "react-icons/io5";
+
+const MyPageTemplate = ({ userDetails }) => {
+  return (
+    <>
+      <div className="userProfile-navigation mt-4 flex">
+        <Button as={Link} to="/" className="mb-4">
+          <AiOutlineHome size={24} />
+        </Button>
+        <Button as={Link} to="/userinfo/settings" className="mb-4 ml-4">
+          <IoSettingsOutline size={24} />
+        </Button>
+      </div>
+
+      <div className="userProfile-infoSection flex flex-col items-center justify-center">
+        <Photo
+          src={"https://picsum.photos/230"}
+          className="userProfile-image my-2 h-60 w-60 rounded-full object-cover"
+        />
+        <div className="userProfile-nickName my-2 text-2xl font-bold">
+          {userDetails?.nickName}
+        </div>
+        <div className="userProfile-email mb-2">
+          <span className="text-gray-400">email: {userDetails?.email}</span>
+        </div>
+      </div>
+
+      <Button
+        as={Link}
+        to="/userinfo/edit"
+        className="userProfile-editButton mb-2 text-lg text-[#FF4800] underline underline-offset-4"
+      >
+        Edit Profile
+      </Button>
+
+      <div className="userProfile-actions mt-4 flex flex-col items-center justify-center">
+        <Button
+          as={Link}
+          to="/userinfo/wishlist/all"
+          className="mb-2 flex h-10 w-60 items-center justify-center rounded-md shadow-md"
+        >
+          My Wishlist
+        </Button>
+        <Button
+          as={Link}
+          to="/userinfo/reviews"
+          className="mb-2 flex h-10 w-60 items-center justify-center rounded-md shadow-md"
+        >
+          My Reviews
+        </Button>
+        <Button
+          as={Link}
+          to="/userinfo/reservations/restaurant"
+          className="mb-2 flex h-10 w-60 items-center justify-center rounded-md shadow-md"
+        >
+          My Reservations
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default MyPageTemplate;
