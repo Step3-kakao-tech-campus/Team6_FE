8a8780e115a2613df8b5f317ed34e76fb8a661e1
Dongjin <ehdwls1638@gmail.com>
2023-11-05 02:30:02 +0900

    feat : implement review form

--- src/components/features/formPages/writeReviewPage/ReviewForm.jsx
+++ src/components/features/formPages/writeReviewPage/ReviewForm.jsx
@@ -1,39 +1,83 @@
 import SectionTitle from "../../../atoms/SectionTitle";
 import { ModalContext } from "../../../../App";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ReservationInfo from "./ReservationInfo";
+import Button from "../../../atoms/Button";
+import StarInput from "./atoms/StarInput";
+import RatingInput from "./organisms/RatingInput";
+import ImageUploader from "./atoms/ImageUploader";
+import ErrorBox from "../../../atoms/ErrorBox";
 
 const ReviewForm = ({ reservation, onSubmit }) => {
+
+  const [score, setScore] = useState(0);
+  const [file, setFile] = useState(""); // 이미지 파일
+  const [errorMsg, setErrorMsg] = useState(null); // 에러 메시지
+  const [reviewText, setReviewText] = useState(""); // 리뷰 텍스트
+  const onChangeReviewText = (e) => {
+    setReviewText(e.target.value);
+  }
   const { hide } = useContext(ModalContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (score === 0) {
+      setErrorMsg("Please select score");
+      return;
+    }
+    onSubmit(e);
+    hide();
+  };
+
   return (
-    <>
-      <div className={"review-form flex flex-col rounded-lg bg-white"}>
+    <div
+      className={
+        "review-form width-flex-layout relative flex flex-col rounded-lg bg-white px-2 gap-4 pb-4"
+      }
+    >
+      <div
+        className={
+          "review-form-header sticky top-0 z-10 flex items-center justify-between bg-white"
+        }
+      >
         <SectionTitle title={"Review"}>Review Form</SectionTitle>
-        <ReservationInfo reservation={reservation} />
-        <form onSubmit={onSubmit} className={"review-form__form flex flex-col"}>
-          <div className={"review-form__form__input"}>
-            <label htmlFor="review">Review</label>
-            <textarea
-              name="review"
-              id="review"
-              cols="30"
-              rows="10"
-              placeholder={"Write your review here"}
-            />
-          </div>
-          <div className={"review-form__form__input"}>
-            <label htmlFor="rating">Rating</label>
-            <input
-              type="number"
-              name="rating"
-              id="rating"
-              placeholder={"Rating"}
-            />
-          </div>
-          <button className={"review-form__form__submit"}>Submit</button>
-        </form>
       </div>
-    </>
+      <ReservationInfo reservation={reservation} />
+      <form
+        onSubmit={onSubmit}
+        className={"review-form-form flex w-full flex-col p-2"}
+      >
+        <div className={"image-upload-wrapper"}>
+          <ImageUploader file={file} setFile={setFile} />
+        </div>
+        <div className={"review-form-text w-full"}>
+          <textarea
+            name="review"
+            id="review"
+            cols="30"
+            rows="10"
+            className={"h-40 w-full rounded-md border-2 border-gray-300 p-2"}
+            placeholder={"Write your review here"}
+            value={reviewText}
+            onChange={onChangeReviewText}
+          />
+        </div>
+        <div className={"review-form-score flex flex-col items-center"}>
+          <RatingInput score={score} setScore={setScore} />
+          <span
+            className={
+              "selected-score text-3xl font-semibold text-tripKoOrange-500"
+            }
+          >
+            {score.toFixed(1)}/5.0
+          </span>
+        </div>
+      </form>
+      {errorMsg && <ErrorBox>{errorMsg}</ErrorBox>}
+      <Button as={"button"} className={"review-form-submit-button p-2 px-4 font-semibold text-lg bg-tripKoOrange text-white rounded-full"} onClick={handleSubmit}>
+        Submit
+      </Button>
+    </div>
   );
 };
 
