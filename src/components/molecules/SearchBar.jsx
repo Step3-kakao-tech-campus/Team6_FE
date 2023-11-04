bff00cfd53ad7976d62d65449fefe835ae8721bd
udadai <yujeong470@gmail.com>
2023-11-05 01:48:32 +0900

    feat : add SideBar

--- src/components/molecules/SearchBar.jsx
+++ src/components/molecules/SearchBar.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../atoms/Input";
 import Button from "../atoms/Button";
 import { FaBars } from "react-icons/fa6";
 import UserAvatar from "../atoms/UserAvatar";
 import MapIcon from "../atoms/MapIcon";
+import SideBar from "../organisms/SideBar";
 
 const SearchBar = ({ value, onChange, onSearch }) => {
   const navigate = useNavigate();
@@ -15,15 +17,24 @@ const SearchBar = ({ value, onChange, onSearch }) => {
     }
   };
 
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+
+  const toggleSideBar = () => setIsSideBarOpen(!isSideBarOpen);
+
   return (
-    <div className="search-section relative flex items-center gap-2 p-2 justify-between">
-      <Button onClick={() => console.log("to sideBar")}>
-        <FaBars size={28} />
-      </Button>
-      <div className={"input-wrapper relative rounded-full w-[calc(100%-6rem)]"}>
-      <div className="absolute flex justify-center items-center h-full left-2">
-        <MapIcon size={20} color={"#FF4800"} />
+    <div className="search-section relative flex items-center justify-between gap-2 p-2">
+      <div className="sidebar-button relative">
+        <Button onClick={toggleSideBar}>
+          <FaBars size={28} />
+        </Button>
+        <SideBar isOpen={isSideBarOpen} toggle={toggleSideBar} />
       </div>
+      <div
+        className={"input-wrapper relative w-[calc(100%-6rem)] rounded-full"}
+      >
+        <div className="absolute left-2 flex h-full items-center justify-center">
+          <MapIcon size={20} color={"#FF4800"} />
+        </div>
         <Input
           className="text-md w-full rounded-lg bg-zinc-200 p-2 pl-10 outline-none"
           type="search"
