2530d584a8917fa95bae1971324cebf57d085a3a
Dongjin <ehdwls1638@gmail.com>
2023-11-05 00:06:57 +0900

    feat : add rating input

---
+++ src/components/features/formPages/writeReviewPage/atoms/StarEmpty.jsx
@@ -0,0 +1,5 @@
+import {FaStar} from "react-icons/fa";
+import {STAR_SIZE} from "./utils";
+
+
+export const StarEmpty = () => <FaStar size={STAR_SIZE} color={"e4e5e9"} />
---
+++ src/components/features/formPages/writeReviewPage/organisms/RatingInput.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { getStarOn } from "../atoms/utils";
+import StarInput from "../atoms/StarInput";
+
+const RatingInput = ({ score, setScore }) => {
+  const [array, setArray] = useState([1, 2, 3, 4, 5]);
+
+  return (
+    <div className="rating-input flex justify-center">
+      {array.map((item) => {
+        return (
+          <>
+            <StarInput
+              on={getStarOn(score, item)}
+              setValue={setScore}
+              index={item}
+            />
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default RatingInput;
