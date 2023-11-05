fdfc01b8c6a3cd1c10aaae0964aaf8df3484e798
Dongjin <ehdwls1638@gmail.com>
2023-11-05 22:27:17 +0900

    feat : add dropdown component

---
+++ src/components/molecules/Dropdown.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+/**
+ * Dropdown component
+ * @param options {string[]} - options to be shown in dropdown
+ * @param value {string} - selected value of dropdown
+ * @param onChange {function} - function to be called when dropdown value is changed
+ * @returns {JSX.Element} - dropdown component
+ * @constructor
+ */
+const Dropdown = ({ options, value, onChange}) => {
+  return (
+    <select
+      name={"dropdown"}
+      className={"dropdown"}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {options.map((option) => (
+        <option value={option}>{option}</option>
+      ))}
+    </select>
+  );
+};
+
+export default Dropdown;
