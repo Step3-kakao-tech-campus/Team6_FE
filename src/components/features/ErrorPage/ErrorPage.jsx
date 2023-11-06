8cb35aa8d406308917dc8f2b1d23f5c6920741a7
udadai <yujeong470@gmail.com>
2023-11-06 19:23:09 +0900

    style: style the ErrorPage

--- src/components/features/ErrorPage/ErrorPage.jsx
+++ src/components/features/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>404 Error</h1>
-      <p>Page not found</p>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="text-center">
+        <p className="text-2xl text-gray-800">404 Error Page not found</p>
+        <h1 className="my-4 text-5xl font-bold text-red-500">Oops!</h1>
+        <p className="mb-2 text-gray-700">
+          The page you are looking for does not exist.
+        </p>
+        <p className="mb-8 text-gray-600">
+          Don't panic, just click on the big button to go HOME!
+        </p>
+        <Link
+          to={"/"}
+          className="rounded bg-tripKoOrange px-6 py-3 font-semibold text-white transition duration-300 hover:bg-blue-700"
+        >
+          HOME
+        </Link>
+      </div>
     </div>
   );
 };
