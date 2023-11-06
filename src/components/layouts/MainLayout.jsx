f419e92ef0d3c954d921c234ab21531b1b5a2328
Dongjin <ehdwls1638@gmail.com>
2023-11-06 23:10:54 +0900

    feat : delete nav bar indicator

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -4,21 +4,20 @@ import { createContext, useState } from "react";
 
 export const NavContext = createContext();
 
-const MainLayout = ({navIndicate}) => {
-  const [activatedTab, setActivatedTab] = useState(1);
-  return (
-    <NavContext.Provider value={{ activatedTab, setActivatedTab }}>
-      <div
-        className={
-          "main-layout width-flex-layout flex flex-col items-center shadow-lg"
-        }
-      >
-        <Outlet />
-          {/* TODO: redux를 이용한 navbar 관리, 새로고침과 같은 상황에서도 대응이 가능하며 contextProvider보다 효과적입니다!*/}
-        <BottomNavBar activatedTab={activatedTab} />
-      </div>
-    </NavContext.Provider>
-  );
+const MainLayout = () => {
+    const [activatedTab, setActivatedTab] = useState(1);
+    return (
+        <NavContext.Provider value={{ activatedTab, setActivatedTab }}>
+            <div
+                className={
+                    "main-layout width-flex-layout flex flex-col items-center shadow-lg"
+                }
+            >
+                <Outlet />
+                <BottomNavBar activatedTab={activatedTab} />
+            </div>
+        </NavContext.Provider>
+    );
 };
 
 export default MainLayout;
