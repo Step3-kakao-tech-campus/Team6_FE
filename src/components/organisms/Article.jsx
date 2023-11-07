f84ff739b32a4297d65f12283cb1247bc113ef7f
Dongjin <ehdwls1638@gmail.com>
2023-11-07 23:59:54 +0900

    fix : apply conditional rendering

--- src/components/organisms/Article.jsx
+++ src/components/organisms/Article.jsx
@@ -1,17 +1,17 @@
-import SectionTitle from "../atoms/SectionTitle";
 import Carousel from "../features/carousel/Carousel";
 import { imagesToSlides } from "../features/carousel/utils";
 
 const Article = ({ description, images }) => {
   return (
     <>
-      <SectionTitle title={"Information"} />
-      <div className={"detail-content-container px-2"}>
-        {description}
-      </div>
-      <div className={"carousel-wrapper height-flex-layout-medium"}>
-        <Carousel slides={imagesToSlides(images)} />
-      </div>
+      {description && (
+        <div className={"detail-content-container px-2"}>{description}</div>
+      )}
+      {images && (
+        <div className={"carousel-wrapper height-flex-layout-medium"}>
+          <Carousel slides={imagesToSlides(images)} />
+        </div>
+      )}
     </>
   );
 };
