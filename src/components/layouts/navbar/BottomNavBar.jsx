ea52af2497370f644b6f349d94153f6532e4adc5
udadai <yujeong470@gmail.com>
2023-11-07 16:28:41 +0900

    fix : indicator when refresh

--- src/components/layouts/navbar/BottomNavBar.jsx
+++ src/components/layouts/navbar/BottomNavBar.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import NavItem from "./NavItem";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BiFoodMenu, BiMap } from "react-icons/bi";
@@ -16,40 +18,65 @@ const getLabelColor = (id, activatedTab) => {
 };
 
 const BottomNavBar = ({ activatedTab }) => {
+  const location = useLocation();
+  const [localActivatedTab, setLocalActivatedTab] = useState(activatedTab);
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setLocalActivatedTab(1);
+    } else if (location.pathname.includes("/foods")) {
+      setLocalActivatedTab(2);
+    } else if (location.pathname.includes("/userinfo/wishlist/all")) {
+      setLocalActivatedTab(3);
+    } else if (
+      location.pathname.includes("/userinfo/reservations/restaurant")
+    ) {
+      setLocalActivatedTab(4);
+    }
+  }, [location]);
+
   return (
     <nav className="bottom-nav-bar width-flex-layout fixed bottom-0 z-[20] flex justify-around divide-x divide-gray-300 rounded-t-2xl bg-white py-2 shadow-2xl drop-shadow">
       {/*<NavIndicator activatedTab={activatedTab} />*/}
       <NavItem
         id={1}
         to={"/"}
-        icon={<BiMap size={30} color={getIconColor(1, activatedTab)} />}
+        icon={<BiMap size={30} color={getIconColor(1, localActivatedTab)} />}
         label={"Spots"}
-        labelColor={getLabelColor(1, activatedTab)}
+        labelColor={getLabelColor(1, localActivatedTab)}
       />
       <NavItem
         id={2}
         to={"/foods"}
-        icon={<BiFoodMenu size={30} color={getIconColor(2, activatedTab)} />}
+        icon={
+          <BiFoodMenu size={30} color={getIconColor(2, localActivatedTab)} />
+        }
         label={"Foods"}
-        labelColor={getLabelColor(2, activatedTab)}
+        labelColor={getLabelColor(2, localActivatedTab)}
       />
       <NavItem
         id={3}
         to={"/userinfo/wishlist/all"}
         icon={
-          <AiOutlineHeart size={30} color={getIconColor(3, activatedTab)} />
+          <AiOutlineHeart
+            size={30}
+            color={getIconColor(3, localActivatedTab)}
+          />
         }
         label={"Wishlist"}
-        labelColor={getLabelColor(3, activatedTab)}
+        labelColor={getLabelColor(3, localActivatedTab)}
       />
       <NavItem
         id={4}
         to={"/userinfo/reservations/restaurant"}
         icon={
-          <HiOutlineTicket size={30} color={getIconColor(4, activatedTab)} />
+          <HiOutlineTicket
+            size={30}
+            color={getIconColor(4, localActivatedTab)}
+          />
         }
         label={"Reservations"}
-        labelColor={getLabelColor(4, activatedTab)}
+        labelColor={getLabelColor(4, localActivatedTab)}
       />
     </nav>
   );
