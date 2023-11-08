8aff34da669a3304bca7f1e2e3e7ad639737fa98
Dongjin <ehdwls1638@gmail.com>
2023-11-09 02:18:34 +0900

    refactor : extract review form duplicated

--- src/components/molecules/cards/ReservationCard.jsx
+++ src/components/molecules/cards/ReservationCard.jsx
@@ -6,7 +6,7 @@ import { getReserveText } from "../../features/reservationListPage/utils";
 import { useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { ModalContext } from "../../../App";
-import ReviewForm from "../../features/formPages/writeReviewPage/ReviewForm";
+import ReviewFormReservation from "../../features/formPages/writeReviewPage/ReviewFormReservation";
 import { useQuery } from "react-query";
 import { getIsReviewed } from "../../../apis/review";
 import Button from "../../atoms/Button";
@@ -74,7 +74,7 @@ const ReservationCard = ({ reservation, reviewable }) => {
               }
               onClick={(e) => {
                 e.stopPropagation();
-                show(<ReviewForm reservation={reservation} />);
+                show(<ReviewFormReservation reservation={reservation} />);
               }}
             >
               Write Review
@@ -85,7 +85,7 @@ const ReservationCard = ({ reservation, reviewable }) => {
               className={"reservation-bottom cursor-pointer text-tripKoOrange-500 underline"}
               onClick={(e) => {
                 e.stopPropagation();
-                show(<ReviewForm reservation={reservation} />);
+                show(<ReviewFormReservation reservation={reservation} />);
               }}
             >
               Modify Review
