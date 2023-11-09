7abde4276a93e592a3bf588e03c9b81c3a3c9ed5
Dongjin <ehdwls1638@gmail.com>
2023-11-09 23:32:46 +0900

    feat : update ReviewCard for infinity scroll

--- src/components/molecules/cards/ReviewCards.jsx
+++ src/components/molecules/cards/ReviewCards.jsx
@@ -1,17 +1,42 @@
 import ReviewCard from "./ReviewCard";
+import useFetchReview from "../../../hooks/useFetchReview";
+import { useMemo } from "react";
 
-const ReviewCards = ({ reviews }) => {
+const ReviewCards = ({ placeId, count }) => {
+  const { reviews, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useFetchReview(placeId);
+
+  const isFull = useMemo(
+    () => reviews.length > count && count !== undefined,
+    [count, reviews?.length],
+  );
+
+  const isMoreReview = useMemo(
+    () => !isFull && hasNextPage,
+    [hasNextPage, isFull],
+  );
+
   return (
     <div className={"flex flex-col gap-4 px-2"}>
-      {reviews.map((review, index) => (
+      {reviews.slice(0, count)?.map((review, index) => (
         <ReviewCard review={review} key={index} />
       ))}
-      {reviews.length === 0 && (
-        <div className={"flex justify-center items-center h-20"}>
-            <h1 className={"text-xl text-gray-400"}>There is no review</h1>
+      {isMoreReview && (
+        <div className={"flex h-20 items-center justify-center"}>
+          <button
+            className={"text-xl text-gray-400 hover:text-gray-500"}
+            onClick={fetchNextPage}
+            disabled={isFetchingNextPage}
+          >
+            {isFetchingNextPage ? "Loading more..." : "Load More"}
+          </button>
         </div>
-        )
-      }
+      )}
+      {!isMoreReview && !isFull && (
+        <div className={"flex h-20 items-center justify-center"}>
+          <h1 className={"text-xl text-gray-400"}>There is no more review</h1>
+        </div>
+      )}
     </div>
   );
 };
