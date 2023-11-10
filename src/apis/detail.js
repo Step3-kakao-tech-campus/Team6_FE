5dc8fd3158bb9fbd9776e802a67ea5c01849f17d
Dongjin <ehdwls1638@gmail.com>
2023-11-11 00:58:40 +0900

    fix : review api rollback

--- src/apis/detail.js
+++ src/apis/detail.js
@@ -3,24 +3,21 @@ import instance from "./api";
 export const getRestaurantById = async (id) => {
   return await instance
     .get(`/restaurant/${id}`)
-    .then((response) => {
-        // console.log("response.data", response.data)
-      return response.data;
-    })
+    .then((response) => response.data.response)
     .catch((error) => Promise.reject(error));
 };
 
 export const getFestivalById = async (id) => {
   return await instance
     .get(`/festival/${id}`)
-    .then((response) => response.data)
+    .then((response) => response.data.response)
     .catch((error) => Promise.reject(error));
 };
 
 export const getSpotById = async (id) => {
   return await instance
     .get(`/touristSpot/${id}`)
-    .then((response) => response.data)
+    .then((response) => response.data.response)
     .catch((error) => Promise.reject(error));
 };
 
