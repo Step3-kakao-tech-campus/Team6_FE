472b1bd47f72c5eb27f578eb66dafa33c821dbdd
Dongjin <ehdwls1638@gmail.com>
2023-11-11 13:45:29 +0900

    feat : add no review sign

--- src/components/molecules/cards/ReviewCards.jsx
+++ src/components/molecules/cards/ReviewCards.jsx
@@ -2,11 +2,18 @@ import ReviewCard from "./ReviewCard";
 
 const ReviewCards = ({ reviews }) => {
   return (
-      <div className={"flex flex-col gap-4 px-2"}>
-        {reviews.map((review, index) => (
-            <ReviewCard review={review} key={index} />
-        ))}
-      </div>
+    <div className={"flex flex-col gap-4 px-2"}>
+      {reviews.length === 0 && (
+        <div className={"flex w-full justify-center pt-3"}>
+          <span className={"text-xl text-gray-700"}>
+            There are no reviews yet. Be the first to review this
+          </span>
+        </div>
+      )}
+      {reviews.map((review, index) => (
+        <ReviewCard review={review} key={index} />
+      ))}
+    </div>
   );
 };
 
